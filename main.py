f358f3d5a111f18bdebc4adddf93e425b86db99b
Austin Salcedo <salcedoaustin@gmail.com>
2026-09-05 20:12:25 -0700

    add main function
    Quick test:
    ```
    (.venv) python main.py
    Hello, Reddit Stock Trend Tracker (from the main function)!
    ```

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello, Reddit Stock Trend Tracker!")
+def main():
+    # Your core program logic goes here
+    print("Hello, Reddit Stock Trend Tracker (from the main function)!")
+
+if __name__ == "__main__":
+    main()
